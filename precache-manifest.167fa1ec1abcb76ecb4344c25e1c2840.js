dccc2ec818d88bbbd871b8783413f3b3a5238666
Kuliza-447 <kuliza-447@Kuliza-447.local>
2020-08-16 01:18:04 +0530

    Updates

---
+++ precache-manifest.167fa1ec1abcb76ecb4344c25e1c2840.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f736f5fbe452acc86389d520214800ab",
+    "url": "/react-ecommerce/index.html"
+  },
+  {
+    "revision": "bb2379846864e13e0741",
+    "url": "/react-ecommerce/static/css/main.a6183742.chunk.css"
+  },
+  {
+    "revision": "6fd2b579cf6c4a81a7eb",
+    "url": "/react-ecommerce/static/js/2.b7d27354.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-ecommerce/static/js/2.b7d27354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb2379846864e13e0741",
+    "url": "/react-ecommerce/static/js/main.e1207aaf.chunk.js"
+  },
+  {
+    "revision": "a3ee1ee417a2b55ad8d3",
+    "url": "/react-ecommerce/static/js/runtime-main.118605eb.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/react-ecommerce/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-ecommerce/static/media/logo.5d5d9eef.svg"
+  }
+]);
